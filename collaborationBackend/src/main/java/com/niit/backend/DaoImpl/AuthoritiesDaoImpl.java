67b80909913171ac9f5578e0722e36faffd0ed7c
Ajit kumar <ajitkm.17@gmail.com>
2016-09-08 13:02:33 +0530

    third commit

--- collaborationBackend/src/main/java/com/niit/backend/DaoImpl/AuthoritiesDaoImpl.java
+++ collaborationBackend/src/main/java/com/niit/backend/DaoImpl/AuthoritiesDaoImpl.java
@@ -1,5 +1,57 @@
 package com.niit.backend.DaoImpl;
 
-public class AuthoritiesDaoImpl {
+import java.util.List;
 
+import org.hibernate.Criteria;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.niit.backend.Dao.AuthoritiesDao;
+import com.niit.backend.model.Authorities;
+
+public class AuthoritiesDaoImpl implements AuthoritiesDao {
+
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	public AuthoritiesDaoImpl(SessionFactory sessionFactory) {
+			this.sessionFactory = sessionFactory;
+
+		}
+	@Transactional
+	public void saveOrUpdate(Authorities authorities) {
+		
+		sessionFactory.getCurrentSession().saveOrUpdate(authorities);
+
+	}
+
+	@Transactional
+	public void editAuthorities(Authorities authorities) {
+		
+		sessionFactory.getCurrentSession().saveOrUpdate(authorities);
+	}
+
+	@Transactional
+	public Authorities get(String authorities_id) {
+		
+		//return sessionFactory.getCurrentSession().get(Authorities.class, authorities_id);
+		String hql = "from Authorities where authorities_id=" + "'" + authorities_id + "'";
+		Query query =(Query) sessionFactory.getCurrentSession().createQuery(hql);
+		List<Authorities> listAuthorities= (List<Authorities>) query.getResultList();
+		if (listAuthorities!= null && !listAuthorities.isEmpty()) {
+			return listAuthorities.get(0);
+		}
+		return null;
+	}
+
+	@Transactional
+	public List<Authorities> list() {
+		
+		@SuppressWarnings("unchecked")
+		List<Authorities> listAuthorities= (List<Authorities>) sessionFactory.getCurrentSession()
+		.createCriteria(Authorities.class).setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY).list();
+		return listAuthorities;
+	}
 }
--- collaborationBackend/src/main/java/com/niit/backend/DaoImpl/ForumPostCommentDaoImpl.java
+++ collaborationBackend/src/main/java/com/niit/backend/DaoImpl/ForumPostCommentDaoImpl.java
@@ -1,5 +1,40 @@
 package com.niit.backend.DaoImpl;
 
-public class ForumPostCommentDaoImpl {
+import java.util.List;
+
+import com.niit.backend.Dao.ForumPostCommentDao;
+import com.niit.backend.model.ForumPostComment;
+
+public class ForumPostCommentDaoImpl implements ForumPostCommentDao {
+
+	@Override
+	public void saveOrUpdate(ForumPostComment forumPostComment) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void editForumPostComment(ForumPostComment forumPostComment) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteForumPostComment(ForumPostComment forumPostComment) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ForumPostComment get(String forumPostComment_id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ForumPostComment> list() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
